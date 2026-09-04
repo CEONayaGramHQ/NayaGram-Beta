e81428643cb4638ad964b843d7ab7ba28f7bd546
𝐂𝐄𝐎𝐍𝐚𝐲𝐚𝐆𝐫𝐚𝐦𝐇𝐐💎 <287420619+CEONayaGramHQ@users.noreply.github.com>
2026-09-04 11:06:10 +0600

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -35,7 +35,7 @@ public static final String APP_HASH = BuildConfig.APP_HASH;
     public static String HUAWEI_STORE_URL = "https://appgallery.huawei.com/app/C101184875";
     public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-81kmi3pi84p11ub8hp9a1funsv0rn2p9.apps.googleusercontent.com";
 
-    public static String HUAWEI_APP_ID = "101184875";
+    public static String HUAWEI_APP_ID = "53333";
 
     // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
     public static boolean IS_BILLING_UNAVAILABLE = false;
